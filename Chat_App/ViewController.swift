1bf84886d710b73d1f1e7abd6ae6c2427a8c6f60
nhantt <nhan.tt@beetechsoft.vn>
2022-12-06 14:06:58 +0700

    Initial Commit

---
+++ Chat_App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Chat_App
+//
+//  Created by m1 on 06/12/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
